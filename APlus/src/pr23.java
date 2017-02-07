345659bd7e0fb0364f15c1de7b7e07e92b90b773
nicklocascio <nicoloc4545@gmail.com>
2017-02-07 14:03:27 -0700

    Heck ya

--- APlus/src/pr23.java
+++ APlus/src/pr23.java
@@ -10,7 +10,11 @@ public class pr23
 		file.nextLine();
 		for(int i = 0; i < times; i++)
 			{
-			
+			double age = file.nextDouble();
+			double dose = file.nextDouble();
+			double childDose = (age/(age+12))*dose;
+			System.out.printf("%.3f", childDose);
+			System.out.println();
 			}
 		}
 	}
