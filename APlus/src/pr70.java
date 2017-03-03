94117363b87a4cb0ab95a0f54bacba0dd9c8a1ad
nicklocascio <nicoloc4545@gmail.com>
2017-03-03 14:03:59 -0700

    hi

---
+++ APlus/src/pr70.java
@@ -0,0 +1,14 @@
+
+public class pr70
+	{
+	public static void main(String [] args)
+		{
+		String word = "PALINDROME";
+		String newWord = "";
+		for()
+			{
+			newWord += word.substring(i, i-1);
+			}
+		System.out.println(newWord);
+		}
+	}
