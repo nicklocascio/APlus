f7fe05de980e2956593395e8c5c95b008a16586c
nicklocascio <nicklocascio@192.168.0.13>
2017-03-09 21:58:48 -0700

    sup

--- APlus/src/pr72.java
+++ APlus/src/pr72.java
@@ -5,30 +5,32 @@ public class pr72
 	{
 	public static void main(String [] args) throws IOException
 		{
-		Scanner file = new Scanner(new File("pr74.txt"));
+		Scanner file = new Scanner(new File("pr72.txt"));
 		int times = file.nextInt();
 		file.nextLine();
 		for(int i = 0; i < times; i++)
 			{
 			String line = file.nextLine();
-			String palindrome = "";
-			String [] original = new String [line.length()];
-	        String [] reverse = new String [line.length()];
-	        for(int j = 0; j < line.length(); j++)
-	        	{
-	        	original[j] = line.substring(j, j+1);
-	        	}
-	        int n = 0;
-	        for(int k = line.length()-1; k >= 0; k--)
-	        	{
-	        	reverse[n] = line.substring(k, k+1);
-	        	n++;
-	        	}
-	        int m = 0;
-	        while(!original[m].equals(reverse[m]))
-	        	{
-	        	
-	        	}
+			String hello = line;
+			String reverse = new StringBuilder(line).reverse().toString();
+			if(line.equals(reverse))
+				{
+				System.out.println(line);
+				}
+			else
+				{
+				for(int j = 0; j < line.length(); j++)
+					{
+					hello = line;
+					hello += reverse.substring(reverse.length()-1-j, reverse.length());
+					String helloR = new StringBuilder(hello).reverse().toString();
+					if(hello.equals(helloR))
+						{
+						System.out.println(hello);
+						break;
+						}
+					}
+				}
 			}
 		}
 	}
